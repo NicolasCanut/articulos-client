9231810049ef1163571b45e8c9d371b2e306993b
Nicolas Canut <nicolas.canut@sbi-technology.com>
2022-03-31 19:52:27 -0300

    create basic container components

---
+++ src/components/containers/Main.tsx
@@ -0,0 +1,12 @@
+import  styled  from 'styled-components';
+
+export const Main = styled.div<{height: string; flexDirection: string; marginTop: string}>`
+    display: flex;
+    flex-direction: ${(props) => props.flexDirection || 'row'};
+    // border: 2px solid;
+    height: ${(props) => props.height};
+    width: 1366px;
+    max-width: 90%;
+    margin: auto;
+    margin-top: ${(props) => props.marginTop};
+`
---
+++ src/components/containers/Wrapper.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background-color: #ffffff;
+    margin: 0;
+    padding: 0;
+    font-family: 'Oceanwide';
+`
