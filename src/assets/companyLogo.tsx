dddf47ec8ff65e884cb6b041b7dd525a4d959681
Nicolas Canut <nicolas.canut@sbi-technology.com>
2022-04-03 04:49:23 -0300

    make company logo reusable

---
+++ src/assets/companyLogo.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ImgLink } from '../components/Typography'
+
+const LogoImg = styled.div.attrs((props: {imgSrc: string}) => props)`
+    width: 100%;
+    height: 100%;
+    background-image: url(${(props) => props.imgSrc});
+    backgrond-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+`
+
+const CompanyLogo = () => {
+  return (
+    <ImgLink href='/' height='100%' width='100%'>
+      <LogoImg imgSrc={require('../assets/img/lanacionLogo.png')}/>
+    </ImgLink>
+  )
+}
+
+export default CompanyLogo
