fac04622c92fd1c57a2ec3efda4979c49681f208
Nicolas Canut <nicolas.canut@sbi-technology.com>
2022-04-03 15:13:47 -0300

    fix company logo display position

--- src/assets/companyLogo.tsx
+++ src/assets/companyLogo.tsx
@@ -6,7 +6,7 @@ const LogoImg = styled.div.attrs((props: {imgSrc: string}) => props)`
     width: 100%;
     height: 100%;
     background-image: url(${(props) => props.imgSrc});
-    backgrond-position: center;
+    background-position: center;
     background-repeat: no-repeat;
     background-size: contain;
 `
